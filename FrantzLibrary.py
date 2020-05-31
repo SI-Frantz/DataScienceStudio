2a30ccf1675750ccaa7bf4f55ad43bc055f7ea03
SI-Frantz <59749756+SI-Frantz@users.noreply.github.com>
2020-05-30 19:05:26 -0700

    Update FrantzLibrary.py
    added dependencies

--- FrantzLibrary.py
+++ FrantzLibrary.py
@@ -1,7 +1,12 @@
+import numpy
+from typing import TypeVar, Callable
+narray = TypeVar('numpy.ndarray')
+import numpy as np
 import spacy
 spnlp = TypeVar('spacy.lang.en.English')  #for type hints
 import os
 os.system("python -m spacy download en_core_web_md")
+!python -m spacy download en_core_web_md
 import en_core_web_md
 nlp = en_core_web_md.load()
 
