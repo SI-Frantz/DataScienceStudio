e64112241acf9fd4ffe87ac37e2b5de3527f68d2
SI-Frantz <59749756+SI-Frantz@users.noreply.github.com>
2020-06-02 20:38:09 -0700

    remove "python -m spacy download en_core_web_md"

--- FrantzLibrary.py
+++ FrantzLibrary.py
@@ -7,7 +7,7 @@ import spacy
 spnlp = TypeVar('spacy.lang.en.English')  #for type hints
 import os
 os.system("python -m spacy download en_core_web_md")
-!python -m spacy download en_core_web_md
+#python -m spacy download en_core_web_md
 import en_core_web_md
 nlp = en_core_web_md.load()
 print("DEBUGGING", type(nlp))
